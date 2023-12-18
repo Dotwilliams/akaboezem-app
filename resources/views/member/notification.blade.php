f2451e661bfdef9b08f0953bb7fb0f36feb13a84
Dotwilliams <abdulaiko16@gmail.com>
2023-12-18 17:24:17 +0100

    list products error

--- resources/views/member/notification.blade.php
+++ resources/views/member/notification.blade.php
@@ -15,7 +15,7 @@
             <div class="d-flex w-100 justify-content-between">
               <h5 class="mb-1">{{$user_notification->title}}</h5>
 
-              <p class="mb-1">{{$user_notification->message}}</p>
+              <p class="mb-1" style="color: red;">{{$user_notification->message}}</p>
               {{-- <small class="text-muted">{{\Carbon\Carbon::parse($user_notification->created_at)->diffForHumans()}}</small> --}}
               {{-- <small class="text-muted">And some muted small print.</small> --}}
             </div>
